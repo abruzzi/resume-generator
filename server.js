5642b37ab3b7b17df42417c67254ac703789e38b
Juntao Qiu <juntao.qiu@gmail.com>
2018-09-14 16:07:54 +1000

    new release

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+// const Puppeteer = require('puppeteer')  
+
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
@@ -6,6 +8,10 @@ const port = process.env.PORT || 3000;
 
 server.use(middlewares);
 server.use(router);
+
+server.get('/download', (req, res) => {
+  res.jsonp(req.query)
+})
 
 server.listen(port, function () {
   console.log('JSON Server is running')
